fa4892623aae650ac41f5d8f2a07305eb258d926
pedrosoaresetec-commits <pedrosoaresetec@gmail.com>
2026-02-27 07:45:48 -0300

    Update exemplo03.php

--- aula03/exemplo03.php
+++ aula03/exemplo03.php
@@ -1,9 +1,35 @@
-<?php 
+<?php
     $x = 4.3;
-    $y = 4.4;
+    $y = 3.5;
     $z = "9.9";
     $n1 = (int) $x + $y;
+    echo   $n1;
+    echo  "<br>";
     $n2 = (int) ($x + $y);
+    echo $n2;
+    echo  "<br>";
+    
     $n3 = (float) ($y + $z);
-    echo "Resultados: $n1, $n2, $n3";
+    echo  $n3;
+    echo "<br>";
+    
+?>
+
+<?php
+   // $a = "Casal 20";
+   // $b = 10;
+   // $c = $a + $b;
+   // echo $c
+?>
+
+<?php
+    $pre = "hiper";
+    echo "${pre}texto";
+?>
+
+<?php
+    $x = “P”;
+    $y = “H”;
+    $z = "$x$y$x";
+    echo $z
 ?>
