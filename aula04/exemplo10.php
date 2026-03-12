9c17e03c3461d49b89fd02098964dd46df6f0fcd
pedrosoaresetec-commits <pedrosoaresetec@gmail.com>
2026-03-12 08:27:53 -0300

    Add files via upload

--- aula04/exemplo10.php
+++ aula04/exemplo10.php
@@ -1,5 +1,11 @@
 <?php
-
+        define ("pi", 3.141592);
+        $raio = 5;
+        $area = 4*pi*$raio**2;
+        echo "Area = $area";
+    
+        $volume = 4/3*pi*$raio**3;
+        echo "Volume"
 
 
 ?>
---
+++ aula04/exemplo11.php
@@ -0,0 +1,19 @@
+<?php
+    $numero1 = 5;
+    $numero2 = 4;
+    $adicao =  $numero1 + $numero2;
+    $subtracao = $numero1 - $numero2;
+    $divisao = $numero1 / $numero2;
+    $multiplicacao = $numero1 * $numero2;
+    $resto = $numero1 % $numero2;
+    
+
+    echo "Numero 1 = $numero1";
+    echo "<br>Numero 2 = $numero2";
+    echo "<br>Adição = $adicao";
+    echo "<br>Subtração = $subtracao";
+    echo "<br>Divisão = $divisao";
+    echo "<br>Multiplicação = $multiplicacao";
+    echo "<br>Resto = $resto";
+
+?>
