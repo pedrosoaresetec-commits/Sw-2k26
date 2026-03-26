df51c0e036d29d551d4a4f35c6eec3b707d67785
Pedro Soares <pedrosoaresetec@gmail.com>
2026-03-26 12:19:55 -0300

    Add files via upload

---
+++ AULA06/restrita.php
@@ -0,0 +1,14 @@
+<?php
+        $nome = $_GET['nome'];
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Área Restrita</title>
+</head>
+<body>
+    <p>Bem vindo <?php echo $nome; ?> a sua Área Restrita</p>
+</body>
+</html>
